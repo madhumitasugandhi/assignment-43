6efa55ddfb51187194f646504c3c49bd57008d2a
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-13 14:52:37 +0530

    All files and Icons added.

--- assignment-43/src/index.jsx
+++ assignment-43/src/index.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home';
+import Add from './components/Add';
+import Show from './components/Show';
+import {Toaster} from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <index />
-  </React.StrictMode>
-);
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
 
-export default index
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/add",
+    element: <Add/>
+  },
+  {
+    path: "/show",
+    element: <Show/>
+  },
+
+  {
+    path: "*",
+    element: <div>404 Not Found</div>
+  }
+])
+
+root.render(
+<div>
+  <RouterProvider router={router}/>
+  <Toaster />
+  </div>
+);
+
+
+export default Index
