ccab026c9fb1a4321184e8260c0d8991b4f2607e
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-14 12:22:48 +0530

    Show file code added.

--- assignment-43/src/components/Show.jsx
+++ assignment-43/src/components/Show.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
-import './Show.css'
-import ImgHome from '../assets/home.png'
-import { Link } from 'react-router-dom'
-import HomeBtn from './HomeBtn/HomeBtn'
 
-const Show = () => {
- return (
+import './Show.css'
+import HomeBtn from './HomeBtn/HomeBtn'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Show() {
+
+  const [notes, setNotes] = React.useState([]);
+  React.useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    setNotes(storedNotes);
+  }, []);
+
+
+
+  return (
     <div className=' subtitle text-primary'>
-      <p>Show Notes</p>
-      <HomeBtn/>
+      <p>Show Notes:</p>
+      <div className='notes-container'>
+        {notes.map((note, index) => (
+          <div key={index} className='note-card'>
+            <h3>{note.title}</h3>
+            <p className='data'>{note.description}</p>
+            <p className='data'>{note.category}</p>
+            <p className='data'>{note.emoji}</p>
+            <button className='btn-danger' onClick={() => {
+              const updatedNotes = notes.filter((_, i) => i !== index);
+              setNotes(updatedNotes);
+              localStorage.setItem("notes", JSON.stringify(updatedNotes));
+            }}>
+              Delete
+              </button>
+          </div>
+        ))}
+        {notes.length === 0 && <p className='text-secondary'>No notes available</p>}
+
+
       </div>
+      <p className='text-secondary'>You can add notes from the Add Notes page.</p>
+
+      <button className='btn btn-primary show-btn' >
+        <Link to="/add" className='link'>
+          Add Notes
+        </Link>
+      </button>
+
+      <HomeBtn />
+    </div >
   )
 }
 
